1803692907c82bc2b0c4e6e28ba1734b6de781f2
Hotaru <biyuehuya@gmail.com>
2024-08-09 17:58:56 +0800

    chore: change some files

--- dist/context/tokens.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class Tokens {
-    static readonly container: unique symbol;
-    static readonly tracker: unique symbol;
-    static readonly record: unique symbol;
-}
-export default Tokens;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { Context } from './context';
-export * from './context';
-export default Context;
